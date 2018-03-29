5354bbb1e7ef57ad2d25bb53151335a6731da853
roderickt1an <roderickt1an@gmail.com>
2018-03-29 15:46:16 +0800

    win-dev init

---
+++ src/components/search/search.vue
@@ -0,0 +1,24 @@
+<!-- search -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' scoped>
+</style>
